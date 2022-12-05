c4e0538be7a61f0f7e905e3ca6f10759be6b6b42
denis-snitko <denissnitko@gmail.com>
2022-12-05 09:58:47 +0300

    feat: counter for second step

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,37 +1,58 @@
 const openStepHandler = () => {
-	const EL_stepBlock = document.querySelectorAll(".js-step");
+  const EL_stepBlock = document.querySelectorAll('.js-step');
 
-	if (EL_stepBlock) {
-		EL_stepBlock.forEach((element) => {
-			element.addEventListener("click", (event) => {
-				if (event.target.classList.contains("js-step-button-open")) {
-					element.classList.toggle("close");
-				}
-			});
-		});
-	}
+  if (EL_stepBlock) {
+    EL_stepBlock.forEach((element) => {
+      element.addEventListener('click', (event) => {
+        if (event.target.classList.contains('js-step-button-open')) {
+          element.classList.toggle('close');
+        }
+      });
+    });
+  }
 };
 
 const activeTab = () => {
-	const EL_tabs = document.querySelectorAll(".tabs__input");
+  const EL_tabs = document.querySelectorAll('.tabs__input');
 
-	if (EL_tabs) {
-		EL_tabs.forEach((currentElement) => {
-			currentElement.addEventListener("click", () => {
-				currentElement
-					.closest(".tabs")
-					.querySelectorAll(".js-tabs-label")
-					.forEach((el) => el.classList.remove("active"));
+  if (EL_tabs) {
+    EL_tabs.forEach((currentElement) => {
+      currentElement.addEventListener('click', () => {
+        currentElement
+          .closest('.tabs')
+          .querySelectorAll('.js-tabs-label')
+          .forEach((el) => el.classList.remove('active'));
 
-				document
-					.querySelector(`[for="${currentElement.id}"]`)
-					.classList.toggle("active");
-			});
-		});
-	}
+        document.querySelector(`[for="${currentElement.id}"]`).classList.toggle('active');
+      });
+    });
+  }
 };
 
-addEventListener("DOMContentLoaded", () => {
-	openStepHandler();
-	activeTab();
+const counter = () => {
+  const EL_counter = document.querySelector('.js-counter');
+  const EL_input = document.querySelector('.js-counter input');
+
+  if (!EL_counter) return;
+
+  let value = Number(EL_input.value.split(' ')[0]);
+  let unit = EL_input.value.split(' ')[1];
+
+  EL_counter.addEventListener('click', (event) => {
+    if (event.target.classList.contains('js-button-minus')) {
+      value = value <= 0 ? 0 : value - 1;
+      EL_input.value = `${value} ${unit}`;
+    }
+
+    if (event.target.classList.contains('js-button-plus')) {
+      value++;
+      EL_input.value = `${value} ${unit}`;
+    }
+  });
+};
+
+addEventListener('DOMContentLoaded', () => {
+  openStepHandler();
+  activeTab();
+  counter();
 });
